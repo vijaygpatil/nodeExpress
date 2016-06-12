3cfa0c8f95648969533d91d8b3444fb29a7e9773
Vijay Patil <vijay.patil@garmin.com>
2016-06-12 11:34:48 -0500

    Use public and src/views as static folders

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(request, response){
-    response.send('Hello World');
+    response.send('Library Application');
 });
 
 app.listen(port, function(err){
