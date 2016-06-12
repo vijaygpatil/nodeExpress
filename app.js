b6857a72b838dc3ff5d8bf9991b96ea52c3f72b6
Vijay Patil <vijay.patil@garmin.com>
2016-06-12 18:03:43 -0500

    Using ejs templating engine

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var app = express();
 var port = 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response){
-    response.send('Library Application');
+    response.render('index', {title: 'Hello from Render EJS', list: ['a', 'b']});
 });
 
 app.listen(port, function(err){
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
