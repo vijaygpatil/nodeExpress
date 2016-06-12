37dba007af45db8e8a5c3214e5dbfe2979a7a00e
Vijay Patil <vijay.patil@garmin.com>
2016-06-12 11:12:33 -0500

    Add app.get and send hello world.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(request, response){
+    response.send('Hello World');
+});
+
 app.listen(port, function(err){
     console.log('Running on port: '+port);
 });
